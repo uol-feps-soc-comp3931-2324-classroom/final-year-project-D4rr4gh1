3eae609eea42ffb5ca7c1416d14ae3bb7cf56f34
Darragh Connolly <darraghconnolly@Darraghs-MacBook-Pro.local>
2024-05-07 01:14:53 +0100

    test fixed

--- MultiState/app/src/test/java/multistate/AppTest.java
+++ MultiState/app/src/test/java/multistate/AppTest.java
@@ -4,6 +4,37 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
+
+    @Test void cellConstructorWorks() {
+        Cell testCell = new Cell(1, 2);
+        assertEquals(testCell.state, 1);
+        assertEquals(testCell.lifespan, 2);
+    }
+
+    @Test void rpsCorrectlyIdentifiesNumberOfLosses() {
+        Cell rock = new Cell(0);
+        Cell paper = new Cell(1);
+        Cell scissors = new Cell(2);
+
+        Cell[][] init = {{rock, rock, paper},
+                         {rock, scissors, rock},
+                         {scissors, paper, rock}};
+        
+        Population testPopulation = new Population(3,3, 0);
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                testPopulation.setCell(i, j, init[i][j]);
+            }
+        }
+        int[] results = testPopulation.playRPS(2, 1, 1);
+        
+        // there are 5 rocks in the neighbourhood, all of which
+        // beat scissors, so the result should be 5
+        assertEquals(results[1], 5);
+    }
+
+
     @Test void playRPSWorks() {
         Cell rock = new Cell(0);
         Cell paper = new Cell(1);
